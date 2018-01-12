daba542e1b30876e8c186b21e50627a29dc0bd93
streanger <pslisz@o2.pl>
2018-01-12 23:27:39 +0100

    gauss modified v1.2

--- get_gauss.py
+++ get_gauss.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 import random
 import matplotlib.pyplot as plt
 from collections import Counter
@@ -7,25 +6,27 @@ import os
 import sys
 from termcolor import colored
 
-def get_gauss(elements=50, top=10, sigma=1, roundValue=2):
+def get_gauss(elements=50, top=10, sigma=1, roundValue=2, distName="Gauss distribution"):
     values = []
     for x in range(elements):
         values.append(round(random.gauss(top, sigma), roundValue))
     values = Counter(values)
     values = list(values.items())
-    sortedValues = sorted(values, key=lambda x: (x[1]))
+    #values = sorted(values, key=lambda x: (x[0]))
+    values.insert(0, distName)
     return values
 
-def draw_chart(data, data2):
-    plt.plot(data, data2)
+def draw_chart(data1, data2):
+    plt.plot(data1, data2)
     plt.ylabel("N pomiarów")
     plt.xlabel("Rozkład wartości")
     plt.grid()
     plt.show()
     #save image in some way
 
-def read_parameters():
-    print("put parameters into file")
+def read_parameters(fileName):
+    parameters = []
+    return parameters
 
 def get_dir(fileName=""):
     #return our current path; if fileName -> return full path
@@ -64,13 +65,42 @@ def write_file(fileName, content, addNewline="\n", response=True, removeSign=[])
         #        print("--< written to: %s" % colored(fileName + " | " + item, item))
     return result
 
-def main(arguments):
-    #print(arguments)
-    values = get_gauss(elements=1000, top=100, sigma=2, roundValue=0)
-    args, value = zip(*values)
+def usage():
+    print('''Example of get_gauss usage:
+--< python3 get_gauss.py parameters.txt fileOut.txt
+--< where parameters lookslike:
+--<  <Name>    <elements>   <top>   <sigma>
+--< param.txt     200        40        2''')
+    print("--< for more go to: %s" % colored("https://github.com/streanger", "cyan"))
+    print("\n" + 16*"<*>" + "\n")
+
+def main(argIn):
+    fileOut = "gauss_data.txt"
+    fileIn = ""
+    if argIn:
+        try:
+            fileIn = argIn[0]
+            fileOut = argIn[1]
+        except:
+            print("--< wrong parameters!")
+            usage()
+    else:
+        usage()
+    parameters = read_parameters(fileIn)
+    for param in parameters:
+        values = get_gauss(elements=500, top=40, sigma=2, roundValue=0)
+        write_file(fileOut,
+                   values,
+                   addNewline=True,
+                   response=True,
+                   removeSign=["(",")"])
+    print("--< finished")
+    #args, value = zip(*values[1:]) #starts from 1 'cause 0 is name
     #draw_chart(args, value)
-    write_file("gauss_data.txt", values, addNewline=True, response=True, removeSign=["(",")"])
 
 
 if __name__=="__main__":
     main(sys.argv[1:])
+
+
+
