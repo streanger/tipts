9ccc2d4b4c155ad61f7246a049f0a71124316a13
streanger <pslisz@o2.pl>
2018-01-21 04:39:19 +0100

    shell_chart.py up3

--- shell_chart.py
+++ shell_chart.py
@@ -1,18 +1,7 @@
 #!/usr/bin/python3
-#simple shell window chart generator by some stranger
+#simple hist chart by some stranger
 import random
 from collections import Counter
-
-
-def shift_list(l, n):
-    return l[n:] + l[:n]
-
-def replace(s, pos, sign):
-    l = list(s)
-    l = l[:pos] + [sign] + l[pos+1:]
-    replaced = "".join(l)
-    return replaced
-    #return "".join(list(s)[:pos]+[sign]+list(s)[pos+1:]) #may cause memory error :) 
 
 def get_gauss(elements=20, top=15, sigma=2):
     values = Counter([round(random.gauss(top, sigma)) for x in range(elements)])
@@ -25,36 +14,33 @@ def some_hist(data=[]):
     if type(data) is dict:
         data = list(data.items())
     xline = get_xline(data)
-    lines = []
     yV = (round(max([x[1] for x in data])//5)+1)*5
     SQ = chr(0x25a0)
+    lines = []
     for x in range(yV):
         yrange = yV-x
-        line = " "*(3-len(str(yV-x))) + str(yrange) + "|" + (len(xline)-5)*" "
+        line = list(" "*(3-len(str(yV-x))) + str(yrange) + "|" + (len(xline)-5)*" ")
         for key, value in data:
             xpos = xline.find(str(key))
             ypos = value
             if ypos >= yrange:  #try with reverse condition
-                line = replace(line, xpos, SQ)
-        lines.append(line)
+                line[xpos] = SQ
+        lines.append("".join(line))
     lines.append(2*" " + "0|" + len(xline)*"_")
     lines.append(xline)
     for line in lines:
         print(line)
 
 def get_xline(data):
-    xvalues = [int(x[0]) for x in data]
+    xvalues = [int(x[0]) for x in data] #int against dictio
     xvalues.sort()
     xline = 5*" "
     for item in xvalues:
         xline += "  " + str(item)
-    if len(xline)<80:
-        return xline
-    else:
-        return ""
+    return xline
 
 
 if __name__=="__main__":
-    gauss = get_gauss(elements=100, top=10, sigma=2)
+    gauss = get_gauss(elements=100, top=25, sigma=2)
     some_hist(data=gauss)  #can put list of list(tuples) or dictio
 
