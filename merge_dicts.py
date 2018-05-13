cde1ec8add6ff9d722db4dc72999ac690bed94fe
streanger <pslisz@o2.pl>
2018-05-14 01:39:12 +0200

    merge_dicts

---
+++ merge_dicts.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def merge_dicts(*dict_args):
+    result = {}
+    for d in dict_args:
+        result.update(d)
+    return result
+
+dictio1 = dict(zip(["first" + str(x) for x in range(3)], [x for x in range(3)]))
+dictio2 = dict(zip(["second" + str(x) for x in range(3)], [x**2 for x in range(3)]))
+
+
+dictOut = merge_dicts(dictio1, dictio2)
+print(dictOut)
