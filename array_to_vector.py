aa1b66627213fd91cb6ae82e6537123ad4b2c59c
streanger <pslisz@o2.pl>
2018-06-05 00:29:23 +0200

    array -> vector upp

--- array_to_vector.py
+++ array_to_vector.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 #some = np.arange(256)
 #some = some.reshape((8,32))
 some = np.random.rand(60,480)
-some = some.flatten(1)
+#some = some.flatten(1)
 
 def draw_chart(data):
     data_x = [key for key, val in enumerate(data)]
@@ -18,10 +18,13 @@ def draw_chart(data):
     plt.show()
     return True
 
-def make_vector(someArray, h):
+def make_vector(someArray):
     #convert 2d, n-size array/matrix to 1d vector
+    h = someArray.shape[0]
+    someArray = someArray.flatten(1)
+    print(h)
     return [mean(someArray[i:i+h]) for i in range(0,len(someArray),h)]
 
-vector = make_vector(some, 60)
+vector = make_vector(some)
 #print(vector)
 draw_chart(vector)
