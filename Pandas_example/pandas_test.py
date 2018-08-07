a0c53e7b5852b6ccb95100a977a0a7e36fa053da
streanger <pslisz@o2.pl>
2018-08-08 00:05:45 +0200

    pandas_example

--- Pandas_example/pandas_test.py
+++ Pandas_example/pandas_test.py
@@ -9,15 +9,27 @@ def script_path():
     os.chdir(path)  #it seems to be quite important
     return path
 
-    
-if __name__ == "__main__":
-    path = script_path()
+def main():    
     some = pd.read_csv("persons.csv", encoding = "ISO-8859-1")
     values = some.values        #as an array
     #print(some)
     ages = some.iloc[:,5]       #get 5th columns
     names_len = [len(item) for item in list(some.iloc[:,0])]
     surnames_len = [len(item) for item in list(some.iloc[:,1])]
+    fig = plt.figure(figsize=(10, 6))
+    ax = fig.gca()
+    ax.set_title("Histogram example")
+
     df = pd.DataFrame({'age': ages, 'surnames_len': surnames_len, 'names_len': names_len})
-    df.plot.hist(alpha=0.65)
+    df.plot.hist(alpha=0.65, ax=ax)
+    ax.set_xlabel("things values")
+    ax.set_ylabel("number of things")
     plt.show()
+    return True
+    
+    
+if __name__ == "__main__":
+    path = script_path()
+    main()
+    
+    
