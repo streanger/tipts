7c159be0fc8e4244070b3ef4fd1429ee7227a560
streanger <pslisz@o2.pl>
2018-09-04 17:50:44 +0200

    read_file up

--- read_file.py
+++ read_file.py
@@ -1,15 +1,25 @@
 import os
 import sys
 
-def read_file(fileName, rmnl=False):
-    path = os.path.realpath(os.path.dirname(sys.argv[0]))
-    path = os.path.join(path, fileName)
+def read_file(path, rmnl=False):
     try:
-        with open(path, "r") as file:
+        with open(path, "r", encoding="utf-8") as file:
             if rmnl:
                 fileContent = file.read().splitlines()
             else:
                 fileContent = file.readlines()
+    except MemoryError:
+        print(">-- memory error")
+        fileContent = []
     except:
         fileContent = []
     return fileContent
+    
+    
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    file = args[0]
+    print(args)
+    content = read_file(file, rmnl=True)
+    print(content)
+    input("\nenter to exit...\n")
