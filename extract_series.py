ae928cb7362288cd890e38a21bdf8d5f1cdd3d04
streanger <pslisz@o2.pl>
2018-12-08 20:28:42 +0100

    Update extract_series.py

--- extract_series.py
+++ extract_series.py
@@ -23,3 +23,40 @@ out = extract_series(some)
 # print(out)
 for item in out:
     print(item)
+    
+# updated 08.12.18, 20:28
+listOfTuples = [(11, 2), (12, 2), (13, 2), (15, 3), (16, 3), (18, 4), (19, 4), (20, 4), (22, 5)]
+
+def extract_tuple_series(data, centerOnly):
+    ''' find series of number and extract them as subslist '''
+    pairs = []
+    pair = []
+    for key, (value, second) in enumerate(data):
+        if not pair:
+            pair.append((value, second))
+        else:
+            if value - pair[-1][0] == 1:
+                pair.append((value, second))
+            else:
+                pairs.append(pair)
+                pair = [(value, second)]
+    if pair:
+        pairs.append(pair)
+    if centerOnly:
+        return [item[len(item)//2] for item in pairs]
+    else:
+        return pairs
+        
+out = extract_tuple_series(listOfTuples, True)
+print("center only:")
+for item in out:
+    print('  ' + str(item))
+out = extract_tuple_series(listOfTuples, False)
+print("series pairs:")
+for item in out:
+    print('  ' + str(item))
+    
+'''
+todo:
+-should work for list of tuples/list with any size, not just with two elements
+'''
