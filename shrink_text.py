95918343d81f5cf8284dd1973faebd3bdddf5e49
streanger <pslisz@o2.pl>
2019-01-31 19:38:19 +0100

    Update shrink_text.py

--- shrink_text.py
+++ shrink_text.py
@@ -2,14 +2,17 @@ import textwrap
 
 def chunk_text(text, charsInLine):
     ''' text wrapper '''
-    wrapper = textwrap.TextWrapper(width=charsInLine, break_long_words=False, replace_whitespace=False)
+    wrapper = textwrap.TextWrapper(width=charsInLine, break_long_words=False, replace_whitespace=True, drop_whitespace=True)
     out = wrapper.wrap(text)
     return out
     
     
 if __name__ == "__main__":
-    text = "someting here and there someting here and there someting here and there someting here and there someting here and there someting here and there someting here and there someting here and there someting here and there someting here and there someting here and there someting here and there someting here and there someting here and there someting here and there someting here and there someting here and there someting here and there someting here and there someting here and there someting here and there someting here and there someting here and there "
+    text = 'This is very strange thing. Your spy bot is just went into the enemy space ship.\nYour mission is to get the secret data and escape from the hostile ship.\nGood luck...'
+    text = text.splitlines()        # need to use it before
     
-    out = chunk_text(text, 40)
-    strContent = '\n'.join(out)
-    print(strContent)
+    # out = '\n'.join(['\n'.join(chunk_text(item, 22)) for item in text])
+    out = '\n '.join(['\n'.join(chunk_text(item, 23)) for item in text])        # space added after true newline
+    # print(out)
+    print(repr(out))
+    
