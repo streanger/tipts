8118bc36beb68c2cfd8bbdb2610b89e14f8d323d
streanger <pslisz@o2.pl>
2019-08-06 22:51:18 +0200

    Add files via upload

--- get_repo_files/get_repo_files.py
+++ get_repo_files/get_repo_files.py
@@ -1,14 +1,27 @@
-''' this is clear script
-    only files are listed
-    no tree
 '''
+    script for count lines of code in github repos
+    no support for files tree
+    extended and simple mode
+    when creating package use following functions:
+        main                    --simple main function (return general info)
+        main_extended           --return dails about files
+        get_all_git_repos       --to get all repos, based on username
+        repo_files              --to get files based on repos list
+        
+    06.08.2019 --> script is ready, to create package from it
+        
+    dd.mm.yyyy --> some info
+        subinfo
+        subinfo
+        
+'''
+
 import sys
 import os
 import urllib.request
 import bs4 as bs
 import requests
 import urllib.parse
-# from urllib.parse import urlparse
 import time
 
 
@@ -41,26 +54,22 @@ def flatten(S):
     return S[:1] + flatten(S[1:])
     
     
-def filter_files(data, filter):
-    out = []
-    return out
-    
-    
 def get_file_from_url(url):
+    '''read file from specified url, to memory'''
     content = urllib.request.urlopen(url).read()
     out = content.decode('utf-8')
     return out
     
     
-def url_destination(url):
-    return urllib.parse.urlparse(url).netloc
-    
-    
 def get_all_git_repos(username, full=False):
-    ''' my own --> https://github.com/streanger '''
+    '''returns list of repos based on specified username'''
     base = 'https://github.com/'
-    url = base + username + '?tab=repositories'
-    # url = urllib.parse.urljoin('https://github.com/', username, '?tab=repositories')  # won't work, as i expected - no time for now, to check it
+    if False:
+        url = base + username + '?tab=repositories'
+    else:
+        # url = urllib.parse.urljoin('https://github.com/', username, '?tab=repositories')  # won't work, as i expected - no time for now, to check it
+        url = urllib.parse.urljoin('https://github.com/', username)
+        url = urllib.parse.urljoin(url, '?tab=repositories')
     content, status = get_content(url)
     soup = bs.BeautifulSoup(content, 'lxml')
     href_tags = soup.find_all(href=True)
@@ -74,9 +83,7 @@ def get_all_git_repos(username, full=False):
         
         
 def list_recursive(repo, current, first):
-    ''' list all files from repo 
-        do it recursively
-    '''
+    '''list all files from repo; do it recursively'''
     base = 'https://github.com/'        # for now it need to cotain this stuff
     begin = repo.replace(base, '', 1)   # only first item
     content, status = get_content(repo)
@@ -111,23 +118,16 @@ def repo_files(repos, toFilter):
     data = {}
     strOut = ''
     for key, repo in enumerate(repos):
-        # input('{:02d}. start with repo: {} '.format(key, repo))
-        # print('{:02d}. {} '.format(key, repo))
-        # data
         full = list_recursive(repo, repo, True)
         out = flatten(full)
         out = list(set(out))       # remove dupli (for now dk why duplicates appears)
         if toFilter:
             out = [item for item in out if item.endswith(toFilter)]
             
-        # change to raw
-        out = [item.replace(base, rawBase).replace('/blob', '', 1) for item in out]
-        
+        out = [item.replace(base, rawBase).replace('/blob', '', 1) for item in out]     # change to raw
         data['{:02d}. {} '.format(key, repo)] = out     # store files from single repo under one key
         strOut += '{:02d}. {} '.format(key, repo) + '\n'
         strOut += '\n'.join(['\t' + item for item in out]) + '\n'*2
-        # print(strOut)
-        # print()
     return data, strOut
     
     
@@ -149,43 +149,58 @@ def count_lines(links, blank):
     return number, strOut
     
     
-def main_with_write(username):
-    ''' think of create some dir as username, and put there .txt files '''
-    # username = 'streanger'
-    # files which are generated: repo_files.txt, links.txt, total.txt, total_log.txt
+def main_extended(username, toFilter=None, blank=True, timer=True):
+    ''' 
+        count lines of github repository, with specified username, files extenions(toFilter),
+        with or without blank lines(blank parameter)
+        timer is optional
+        creates directory with username name and puts there following files:
+        repo_files.txt, links.txt, total.txt, total_log.txt
+        its extended version of main function
+    '''
+    if timer:
+        print('script starts, please wait...')
+        begin = time.time()
+        
+    if not os.path.exists(username):
+        os.makedirs(username)
+    currentPath = os.path.realpath(os.path.dirname(sys.argv[0]))
+    dirPath = os.path.join(currentPath, username)
     
-    print('script starts, please wait...')
-    begin = time.time()
+    
     repos = get_all_git_repos(username, True)
-    # repos = [repos[4], repos[11], repos[18]]
-    
-    # toFilter = ('.py', '.c', '.cpp' , '.txt')
-    toFilter = ('.py', )
+    if not toFilter:
+        toFilter = ()
     data, strOut = repo_files(repos, toFilter)
-    simple_write('repo_files.txt', strOut)
+    simple_write(os.path.join(dirPath, 'repo_files.txt'), strOut)
     
     links = [x for y in list(data.values()) for x in y]
-    simple_write('links.txt', '\n'.join(links))
+    simple_write(os.path.join(dirPath, 'links.txt'), '\n'.join(links))
     
-    blank = True
     totalNumberOfLines, log = count_lines(links, blank)
     out = 'username: {}\ntotalFiles: {}\ntotalNumberOfLines: {}\nfilteredBy: {}\nwithBlankLines: {}'.format(username,
                                                                                                             len(links),
                                                                                                             totalNumberOfLines,
                                                                                                             toFilter,
                                                                                                             blank)
-    simple_write('total.txt', out)
-    simple_write('total_log.txt', log)
-    finish = time.time()
-    print('script execution finished in: {}[s]\nYou can open newly created files'.format(round(finish - begin, 4)))
-    return True
+
+    simple_write(os.path.join(dirPath, 'total.txt'), out)
+    simple_write(os.path.join(dirPath, 'total_log.txt'), log)
+    
+    if timer:
+        finish = time.time()
+        print('script execution finished in: {}[s]\nYou can open newly created files'.format(round(finish - begin, 4)))
+    return out
     
     
-def main(username, toFilter, blank, timer=True):
+def main(username, toFilter=None, blank=True, timer=True):
     if timer:
         print('script starts, please wait...')
         begin = time.time()
+        
     repos = get_all_git_repos(username, True)
+    if not toFilter:
+        toFilter = ()
     data, strOut = repo_files(repos, toFilter)
     links = [x for y in list(data.values()) for x in y]
     totalNumberOfLines, log = count_lines(links, blank)
@@ -196,15 +211,22 @@ def main(username, toFilter, blank, timer=True):
                                                                                                             blank)
     if timer:
         finish = time.time()
-        print('script execution finished in: {}[s]\nYou can open newly created files'.format(round(finish - begin, 4)))
+        print('script execution finished in: {}[s]'.format(round(finish - begin, 4)))
     return out
     
     
 if __name__ == "__main__":
     script_path()
     username = 'streanger'
-    out = main(username, ('.py', ), False)
-    simple_write('{}_total.txt'.format(username), out)
+    
+    # simple main
+    out = main(username, ('.py', ), False, True)
+    print(out)
+    
+    input('\npress enter, to run main_extended...\n')
+    
+    # extended main
+    out = main_extended(username, ('.py', ), False, True)
     print(out)
     
     
