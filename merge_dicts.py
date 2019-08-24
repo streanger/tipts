c0e84d6bdf90f33e208746e1a646b7398bd3de19
streanger <pslisz@o2.pl>
2019-08-24 18:23:23 +0200

    Update merge_dicts.py

--- merge_dicts.py
+++ merge_dicts.py
@@ -12,3 +12,7 @@ dictio2 = dict(zip(["second" + str(x) for x in range(3)], [x**2 for x in range(3
 
 dictOut = merge_dicts(dictio1, dictio2)
 print(dictOut)
+
+# from python3.5
+out = {**dictio1, **dictio2}
+print(out)
