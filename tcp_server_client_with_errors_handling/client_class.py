77e3b169ee7bb0dd6c97cc9ae96e084371ac548e
streanger <pslisz@o2.pl>
2020-05-06 21:03:30 +0200

    Add files via upload
    -added searching for connection lost in thread

--- tcp_server_client_with_errors_handling/client_class.py
+++ tcp_server_client_with_errors_handling/client_class.py
@@ -1,15 +1,21 @@
 import sys
 import time
 import socket
-
+from threading import Thread
 
 
 class TCPClient():
     def __init__(self, target_host, target_port):
         self.target_host = target_host
         self.target_port = target_port
-        self.timeout = 10      # [s]
+        self.timeout = 5      # [s]
+        self.LOCK = False       # lock for retrieving
+        self.CONNECTION_STATUS = True
         self.client = self.make_connection(self.target_host, self.target_port)
+        if not self.client:
+            self.CONNECTION_STATUS = False
+        connection_monitor = Thread(target=self.retrieve_connection)
+        connection_monitor.start()
         
         
     def make_connection(self, target_host, target_port):
@@ -28,14 +34,19 @@ class TCPClient():
         
         
     def send_request(self, msg):
-        if not msg.strip():
-            return '[o] (empty)'
-            
-        response = '[o] (empty)'
+        response = '[o] White characters. Not sent.'
         status = False
         
+        if not msg.strip():
+            return response
+            
+        if self.LOCK:
+            # retrieving connection in progress
+            return '[o] No connection with server.'
+            
+            
         if not self.client:
-            self.client = self.make_connection(self.target_host, self.target_port)
+            # self.client = self.make_connection(self.target_host, self.target_port)
             return '[o] Failed to connect to server'
             
             
@@ -55,23 +66,32 @@ class TCPClient():
             response = '[o] Connection reset error'
             
             
-        if not status:
-            # try to retrieve connection
-            self.client = self.make_connection(self.target_host, self.target_port)
-            
+        self.CONNECTION_STATUS = status
         return response
         
         
-def example():
-    '''
-    just create client and send msg to server
-    '''
-    client = TCPClient('localhost', 9999)
-    response = client.send_request('message')
-    print(response)
-    return None
-    
-    
+    def retrieve_connection(self):
+        '''check if connection is not lost in loop and then try to retrieve it'''
+        while True:
+            # print('looking for connection lost')
+            if self.CONNECTION_STATUS:
+                print("everything's fine")
+                time.sleep(0.1)
+                continue
+                
+            self.LOCK = True
+            
+            while True:
+                print('try to retrieve connection')
+                self.client = self.make_connection(self.target_host, self.target_port)
+                
+                if self.client != False:
+                    self.CONNECTION_STATUS = True
+                    self.LOCK = False
+                    break       # go back to monitoring loop
+        return None
+        
+        
 def loop_example():
     global DEBUG
     DEBUG = True
@@ -82,15 +102,17 @@ def loop_example():
         
         
     client = TCPClient(target_host, target_port)
+    
     while True:
-        msg = input('send something: ')
+        msg = input('put something: ')
         response = client.send_request(msg)
         print(response)
-    return
+        
+    return None
     
     
 if __name__ == "__main__":
-    # loop_example()
-    example()
+    client = TCPClient('localhost', 9999)
+    client.send_request('hello')
     
     
